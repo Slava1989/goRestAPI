c2dfc68850933a56a92c18071500efbee9c5c34a
Slava Chirita <veaceslav.chirita@gmail.com>
2024-11-04 22:58:16 +0200

    Adds Dockerfile

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/Slava1989/goRestAPI/internal/db"
+)
 
 // Run - is going to be responsible for
 // the instantiation and startup of
 // go app
 func Run() error {
 	fmt.Println("starting up")
+
+	db, err := db.NewDatabase()
+
+	if err != nil {
+		fmt.Println("Failed to connect to database")
+		return err
+	}
+
+	if err := db.MigrateDB(); err != nil {
+		fmt.Println("failed to migrate database")
+		return err
+	}
+
+	fmt.Println("successfully connected and pinged database")
+
 	return nil
 }
 
---
+++ internal/db/comment.go
@@ -0,0 +1,43 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"github.com/Slava1989/goRestAPI/internal/comment"
+)
+
+type CommentRow struct {
+	ID string
+	Slug sql.NullString
+	Body sql.NullString
+	Author sql.NullString
+}
+
+func convertCommentRowToComment(c CommentRow) comment.Comment {
+	return comment.Comment{
+		ID: c.ID,
+		Slug: c.Slug.String,
+		Body: c.Body.String,
+		Author: c.Author.String,
+	}
+}
+
+func (d *Database) GetComment(ctx context.Context, uuid string) (comment.Comment, error) {
+	var cmtRow CommentRow
+	row := d.Client.QueryRowContext(
+		ctx,
+		`Select id, slug, body, author
+		From comments
+		Where id = $1`,
+		uuid,
+	)
+	err := row.Scan(&cmtRow.ID, &cmtRow.Slug, &cmtRow.Body, &cmtRow.Author)
+
+	if err != nil {
+		return comment.Comment{}, fmt.Errorf("error fetching the comment by uuid: %w", err)
+	}
+
+	return convertCommentRowToComment(cmtRow), nil
+}
